751f09af8ba4dfd72b2048b45915e7de6f4cc76b
asrar <asrar.rifae@hotmail.com>
2023-01-25 18:54:54 +0300

    TicTacToe Project

--- Designing.py
+++ Designing.py
@@ -1,14 +1,16 @@
 from art import *
 from colorama  import *
+import emoji
 
 
-# HERER WE CALL THE GAME TO START 
+
+# HERE WE CALL THE GAME TO START 
 
 user_name= ""
 user_answer= ""
 
 def greeting()-> str:
-
+    print(emoji.emojize(":person_raising_hand:"))
     user_name = input('Hey There ! , Please Enter your Name: ')
     try:
         if not user_name.isalpha() or user_name == '':
@@ -20,6 +22,9 @@ def greeting()-> str:
         hello =text2art( "Welcome to TicTacToe Game ! " , font='small',chr_ignore=True)
         UserNameDesign =text2art(  user_name , font='small',chr_ignore=True)
         print(Fore.BLUE + hello  , Fore.GREEN + UserNameDesign   )
+    
+
+
 
 greeting()
 
@@ -28,15 +33,15 @@ greeting()
 #_______________________________________________
 
 while True:
-    user_answer=input("Hello,  Would you Like to Play TicTacToe with me ? (Y/N) " )
-    if user_answer.lower() == 'y':
+    user_answer=input("Would you Like to Play TicTacToe with me ? if your answer is yes , you can choose your player (X,O) or exit to end ." )
+    if user_answer.lower() == "X" and "O":
         from TicTacToe import start_new_game 
         break
-    elif user_answer.lower() == 'n':
-        print(Fore.RED + "oh that's bad :( , nice to know you tho !! ^-^ " )
+    elif user_answer.lower() == 'exit' :
+        print(Fore.RED + "Ohh that's bad 😥 , nice to know you tho !!", emoji.emojize(":handshake:"))
         break
     else:
-        print('Type Y/N')
+        print('Please enter a valid input ! ')
 
 
     
